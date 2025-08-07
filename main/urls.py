e0b407674c0dca9f0eca3e298ed8a1bc94306910
unknown <rafasoelistiono@gmail.com>
2025-08-07 10:11:59 +0700

    hubungi-kami

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import beranda
+from .views import hubungi_kami
 
 urlpatterns = [
     path('', beranda, name='beranda'),
+    path('beranda', beranda, name='beranda'),
+    path('hubungi_kami', hubungi_kami, name='hubungi_kami')
 ]
