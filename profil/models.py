226cb1c1100a159fd6b884e4647bafee2996bc8b
sopigoo <shofinadhifa24@gmail.com>
2025-08-13 00:28:59 +0700

    feat: fungsionaris model

--- profil/models.py
+++ profil/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Fungsionaris(models.Model):
+    nama = models.CharField(max_length=100)
+    jabatan = models.CharField(max_length=150)
+    foto = models.ImageField(upload_to='fungsionaris/')
+
+    def __str__(self):
+        return f"{self.nama} - {self.jabatan}"
