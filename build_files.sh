ece76db4f206ffaa160eb1e0c2db327cdb48e125
sopigoo <shofinadhifa24@gmail.com>
2025-08-15 00:25:11 +0700

    chore: fix build files

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
 echo "Building the project..."
+
+# Install pip terlebih dahulu
+python3.9 -m ensurepip --upgrade
+
+# Install dependencies
+python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput --clear
+
+echo "Build completed!"
