b8e6f0d11a566df67ff85de311ffad9d423c8b59
Fideligo <mrafighalibf@gmail.com>
2025-08-14 22:39:27 +0700

    views kegiatan

--- kegiatan/views.py
+++ kegiatan/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Kegiatan
+from datetime import datetime
 
-# Create your views here.
+def kegiatan_all(request):
+    kegiatan_list = Kegiatan.objects.all().order_by('tanggal')
+    return render(request, 'kegiatan_all.html', {'kegiatan_list': kegiatan_list})
+
+def kegiatan_upcoming(request):
+    kegiatan_list = Kegiatan.objects.filter(tanggal__gte=datetime.now()).order_by('tanggal')
+    return render(request, 'kegiatan_upcoming.html', {'kegiatan_list': kegiatan_list})
+
+def kegiatan_past(request):
+    kegiatan_list = Kegiatan.objects.filter(tanggal__lt=datetime.now()).order_by('-tanggal')
+    return render(request, 'kegiatan_past.html', {'kegiatan_list': kegiatan_list})
