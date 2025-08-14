d72d079d6cbf7729f07c5b0337f3f33773c3f300
Fideligo <mrafighalibf@gmail.com>
2025-08-14 22:38:40 +0700

    kegiatan admin

--- kegiatan/admin.py
+++ kegiatan/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Event
+from .models import Kegiatan
 
 # Register your models here.
+
+@admin.register(Kegiatan)
+class KegiatanAdmin(admin.ModelAdmin):
+    list_display = ("judul", "tanggal", "lokasi")
+    list_filter = ("tanggal", "lokasi")
+    search_fields = ("judul", "deskripsi", "lokasi")
+    ordering = ("-tanggal",)
+    date_hierarchy = "tanggal"  
