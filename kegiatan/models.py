1e534c73155e8f820bcbe87db63d6053eb981510
Fideligo <mrafighalibf@gmail.com>
2025-08-14 22:39:05 +0700

    model kegiatan

--- kegiatan/models.py
+++ kegiatan/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Kegiatan(models.Model):
-    judul = models.CharField(max_length=200)
-    deskripsi = models.TextField()
-    tanggal = models.DateTimeField()
-    lokasi = models.CharField(max_length=200)
+    judul = models.CharField(max_length=200, verbose_name="Judul Kegiatan")
+    deskripsi = models.TextField(verbose_name="Deskripsi Kegiatan", blank=True)
+    tanggal = models.DateTimeField(verbose_name="Tanggal Kegiatan")
+    lokasi = models.CharField(max_length=200, verbose_name="Lokasi Kegiatan")
 
     def __str__(self):
         return self.judul
