fe7b946ac743bb07a77e805b865c9531e93458b4
sopigoo <shofinadhifa24@gmail.com>
2025-08-15 00:43:18 +0700

    chore: fix deploy

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 echo "Building the project..."
 
-# Pastikan pip tersedia dan upgrade
-python -m ensurepip --upgrade
-python -m pip install --upgrade pip
+# Install dependencies if not already installed
+if ! command -v pip &> /dev/null; then
+    python3 -m ensurepip
+    python3 -m pip install --upgrade pip
+fi
 
-# Install dependencies
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --noinput --clear
+python3 manage.py collectstatic --noinput --clear
 
 echo "Build completed!"
