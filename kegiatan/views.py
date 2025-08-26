ff36ad2c0db60b9fc3a119197b01c9a068bd1949
Fideligo <mrafighalibf@gmail.com>
2025-08-26 21:55:51 +0700

    views untuk page baru

--- kegiatan/views.py
+++ kegiatan/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 from .models import Kegiatan
 from datetime import datetime
+from django.template import loader
+from django.http import HttpResponse
 
 def kegiatan_all(request):
-    kegiatan_list = Kegiatan.objects.all().order_by('tanggal')
-    return render(request, 'kegiatan_all.html', {'kegiatan_list': kegiatan_list})
+    kegiatan_list = Kegiatan.objects.all()
+    template = loader.get_template("kegiatan_all.html")
+    context = {
+        'kegiatan_list': kegiatan_list,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def kegiatan_upcoming(request):
-    kegiatan_list = Kegiatan.objects.filter(tanggal__gte=datetime.now()).order_by('tanggal')
-    return render(request, 'kegiatan_upcoming.html', {'kegiatan_list': kegiatan_list})
+    kegiatan_list = Kegiatan.objects.all()
+    template = loader.get_template("kegiatan_upcoming.html")
+    context = {
+        'kegiatan_list': kegiatan_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def kegiatan_past(request):
-    kegiatan_list = Kegiatan.objects.filter(tanggal__lt=datetime.now()).order_by('-tanggal')
-    return render(request, 'kegiatan_past.html', {'kegiatan_list': kegiatan_list})
+    kegiatan_list = Kegiatan.objects.all()
+    template = loader.get_template("kegiatan_past.html")
+    context = {
+        'kegiatan_list': kegiatan_list,
+    }
+    return HttpResponse(template.render(context, request))
