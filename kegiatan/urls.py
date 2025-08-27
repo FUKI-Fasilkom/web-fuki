65daeb3ba1f6bc4168fc9fe36fc204ea3e072712
Fideligo <mrafighalibf@gmail.com>
2025-08-27 10:49:59 +0700

    default: upcoming page

--- kegiatan/urls.py
+++ kegiatan/urls.py
@@ -4,8 +4,8 @@ from .views import *
 app_name = 'kegiatan'
 
 urlpatterns = [
-    path('', kegiatan_all, name='home'),
-    path('all/', kegiatan_all, name='kegiatan_all'),
-    path('upcoming/', kegiatan_upcoming, name='kegiatan_upcoming'),
-    path('past/', kegiatan_past, name='kegiatan_past'),
+    path('', kegiatan_upcoming_page, name='home'),
+    path('all/', kegiatan_all_page, name='kegiatan_all'),
+    path('upcoming/', kegiatan_upcoming_page, name='kegiatan_upcoming'),
+    path('past/', kegiatan_past_page, name='kegiatan_past'),
 ]
